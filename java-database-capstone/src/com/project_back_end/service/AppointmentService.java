23d89594babb289e500cec49772d5c7616bda162
Nguyễn Võ Duy Tuân <boy0388402@gmail.com>
2025-10-22 11:50:51 +0700

    Update AppointmentService.java

--- java-database-capstone/src/com/project_back_end/service/AppointmentService.java
+++ java-database-capstone/src/com/project_back_end/service/AppointmentService.java
@@ -8,6 +8,32 @@ package com.project_back_end.service;
  *
  * @author admin
  */
+
+import com.project_back_end.models.Appointment;
+import com.project_back_end.repositories.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class AppointmentService {
-    
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    // Đặt lịch mới
+    public Appointment bookAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    // Lấy danh sách lịch hẹn của bác sĩ theo ngày
+    public List<Appointment> getAppointmentsForDoctorOnDate(int doctorId, LocalDate date) {
+        return appointmentRepository.findByDoctorIdAndAppointmentTimeBetween(
+            doctorId,
+            date.atStartOfDay(),
+            date.plusDays(1).atStartOfDay()
+        );
+    }
 }
