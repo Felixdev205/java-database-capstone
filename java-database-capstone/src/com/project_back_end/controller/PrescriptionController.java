43c5824dcd44ef20d6f6972b0c3f4f7e43190de2
Nguyễn Võ Duy Tuân <boy0388402@gmail.com>
2025-10-22 11:51:33 +0700

    Update PrescriptionController.java

--- java-database-capstone/src/com/project_back_end/controller/PrescriptionController.java
+++ java-database-capstone/src/com/project_back_end/controller/PrescriptionController.java
@@ -8,6 +8,32 @@ package com.project_back_end.controller;
  *
  * @author admin
  */
+
+
+import com.project_back_end.models.Prescription;
+import com.project_back_end.services.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/prescriptions")
 public class PrescriptionController {
     
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @PostMapping
+    public ResponseEntity<?> createPrescription(
+            @RequestHeader("Authorization") String token,
+            @Validated @RequestBody Prescription prescription) {
+        if (!"valid-token123".equals(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+        Prescription saved = prescriptionService.save(prescription);
+        return ResponseEntity.ok(saved);
+    }
 }
+
